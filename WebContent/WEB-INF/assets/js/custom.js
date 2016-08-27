8b5034b7b4814206bb499c7a4dbcda3766c81b56
akhal <akhal@DESKTOP-08CNQGS>
2016-08-27 15:26:39 -0400

    change panel css

--- WebContent/WEB-INF/assets/js/custom.js
+++ WebContent/WEB-INF/assets/js/custom.js
@@ -1,12 +1,48 @@
-$(document).ready(function(){
-	$(document).on("click","nav", function(){
-		var n={
-			type	: "info",
+var counter=0;
+var n={
+		type	: "info",
+		title 	: "My Error",
+		message : "heeeeeeeeeeeha",
+		timeOut : 15000
+	};
+	var l={
+			type	: "danger",
 			title 	: "My Error",
 			message : "heeeeeeeeeeeha",
 			timeOut : 15000
 		};
-		notification(n);
+	var k={
+			type	: "warning",
+			title 	: "My Error",
+			message : "heeeeeeeeeeeha",
+			timeOut : 15000
+		};
+	var i={
+			type	: "primary",
+			title 	: "My Error",
+			message : "heeeeeeeeeeeha",
+			timeOut : 15000
+		};
+	
+$(document).ready(function(){
+	$(document).on("click","nav", function(){
+		
+		switch(counter){
+		case 0:
+			notification(n);
+			break;
+		case 1:
+			notification(l);
+			break;
+		case 2:
+			notification(k);
+			break;
+			notification(i);
+		case 3:
+			break;
+		}
+		counter++;
+		counter=counter%4;
 	});
 
 })
